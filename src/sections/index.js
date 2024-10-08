de04e345837c2891f388425584ba8c791ea15c18
madxrebel <jahanzaiblucky787@gmail.com>
2024-10-08 22:08:03 +0500

    added nike-app

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Footer from './Footer'
+import Hero from './Hero'
+import SuperQuality from './SuperQuality'
+import Services from './Services'
+import PopularProducts from './PopularProducts'
+import Subscribe from './Subscribe'
+import CustomerReviews from './CustomerReviews'
+import SpecialOffers from './SpecialOffers'
+
+export {
+    Footer,
+    Hero,
+    SuperQuality,
+    PopularProducts,
+    Services,
+    Subscribe,
+    CustomerReviews,
+    SpecialOffers
+}
